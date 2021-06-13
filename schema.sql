d9e73eb422d43cf890fec3ccfda9dd36af417a84
Syeda Hasan <38924096+rimpi1612@users.noreply.github.com>
2021-06-13 01:08:06 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,9 @@
+-- DB Name rental_db
 -- Drop table
 
 DROP TABLE IF EXISTS isd_capita;
 
-CREATE TABLE public.isd_capita (
+CREATE TABLE isd_capita (
 	"year" varchar(32) NULL,
 	rate int4 NULL
 );
